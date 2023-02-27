7fd53f62b9a2c0dcf9c01debc95ae4caf0f8bc9e
than-dev <nathan.cotrim@gmail.com>
2023-02-27 12:52:41 -0300

    fix: format server response to json

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createServer } from 'node:http';
 const server = createServer(async (req, res) => {
 	if (req.method === 'POST') {
 		await createPost();
-		return res.end({ message: 'article created with success' });
+		return res.end(JSON.stringify({ message: 'article created with success' }));
 	} else {
 		res.end();
 	}
