b41c27bc1ea931d08a83e3591e17311caea7edae
than-dev <nathan.cotrim@gmail.com>
2023-02-27 06:07:09 -0300

    fix: await all tasks before return

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import { createPost } from './create-post.js';
 const server = createServer(async (req, res) => {
 	if (req.method === 'POST') {
 		await createPost();
+		res.end();
+	} else {
+		res.end();
 	}
-
-	res.end();
 });
 
 server.listen(process.env.PORT || 3000);
