318a1472225cae54be6d1a6c31eb66a59fef7085
than-dev <nathan.cotrim@gmail.com>
2023-02-27 05:36:49 -0300

    refactor: set feature and server into same file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import 'dotenv/config';
 
 import { createServer } from 'node:https';
-import { createPost } from './create-post.js';
+
+import { generateBlogPost, generatePostTheme } from './services/openai.js';
+import { setUsedTheme, verifyThemeWasUsed } from './services/themes.js';
+import { createMediumPost } from './services/medium.js';
+
+async function createPost() {
+	const theme = await generatePostTheme();
+
+	const themeWasUsed = await verifyThemeWasUsed(theme);
+
+	if (themeWasUsed) {
+		return await createPost();
+	}
+
+	console.log('\n✅ theme generated');
+
+	const content = await generateBlogPost(theme);
+
+	await createMediumPost({ content, theme });
+
+	await setUsedTheme(theme);
+}
 
 const server = createServer(async (req, res) => {
 	if (req.method === 'POST') {
