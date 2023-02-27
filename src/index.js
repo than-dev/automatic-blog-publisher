9cb7d01aa785e5aff74b727d0adb6a92b4360153
than-dev <nathan.cotrim@gmail.com>
2023-02-27 16:40:05 -0300

    refactor: cron job time

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { CronJob } from 'cron';
 
 import { createPost } from './create-post.js';
 
-const job = new CronJob(`0 16 * * *`, createPost);
+const job = new CronJob(`0 17 * * *`, createPost);
 job.start();
