a7685e755e23903764b2e47d2e48307fda5646f4
than-dev <nathan.cotrim@gmail.com>
2023-02-27 06:04:24 -0300

    refactor: change https to http

---
+++ src/create-post.js
@@ -0,0 +1,23 @@
+import { generateBlogPost, generatePostTheme } from './services/openai.js';
+import { setUsedTheme, verifyThemeWasUsed } from './services/themes.js';
+import { createMediumPost } from './services/medium.js';
+
+async function createPost() {
+	const theme = await generatePostTheme();
+
+	const themeWasUsed = await verifyThemeWasUsed(theme);
+
+	if (themeWasUsed) {
+		return await createPost();
+	}
+
+	console.log('\n✅ theme generated');
+
+	const content = await generateBlogPost(theme);
+
+	await createMediumPost({ content, theme });
+
+	await setUsedTheme(theme);
+}
+
+export { createPost };
--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
 import 'dotenv/config';
 
-import { createServer } from 'node:https';
-
-import { generateBlogPost, generatePostTheme } from './services/openai.js';
-import { setUsedTheme, verifyThemeWasUsed } from './services/themes.js';
-import { createMediumPost } from './services/medium.js';
-
-async function createPost() {
-	const theme = await generatePostTheme();
-
-	const themeWasUsed = await verifyThemeWasUsed(theme);
-
-	if (themeWasUsed) {
-		return await createPost();
-	}
-
-	console.log('\n✅ theme generated');
-
-	const content = await generateBlogPost(theme);
-
-	await createMediumPost({ content, theme });
-
-	await setUsedTheme(theme);
-}
+import { createServer } from 'node:http';
+import { createPost } from './create-post.js';
 
 const server = createServer(async (req, res) => {
 	if (req.method === 'POST') {
