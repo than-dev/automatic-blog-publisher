8fd4f6e72a4cbb2a47dda654dccb996118c84c55
than-dev <nathan.cotrim@gmail.com>
2023-02-27 14:53:54 -0300

    refactor: remake topics config

--- src/config.js
+++ src/config.js
@@ -1,16 +1,11 @@
 const topics = [
-	'good development pratices',
-	'clean code',
 	'javascript',
-	'efficience as developer',
 	'react',
 	'flutter',
 	'typescript',
 	'nodejs',
-	'programming tips',
 	'html',
-	'css',
-	'technology career'
+	'css'
 ];
 
 export { topics };
