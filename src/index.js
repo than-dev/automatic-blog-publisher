58481fb46930bd5c823d2071bd0476e9b1f49952
than-dev <nathan.cotrim@gmail.com>
2023-02-27 15:52:00 -0300

    fix> import according es modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { CronJob } from 'cron';
 
-import { createPost } from './create-post';
+import { createPost } from './create-post.js';
 
 const job = new CronJob(`0 16 * * *`, createPost);
 job.start();
