ec3be1c2d0c0d7e33668d864972f4ab5ffa80317
than-dev <nathan.cotrim@gmail.com>
2023-02-28 05:13:24 -0300

    test: keep server on by http

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import { createPost } from './create-post.js';
+import { createServer } from 'node:http';
+
+const server = createServer(async (req, res) => {
+	res.end();
+});
+
+server.listen(3000);
 
 setInterval(createPost, 1000 * 60 * 60 * 24);
