10cdbbe54e579cf12d5054a2f82c251451d37751
than-dev <nathan.cotrim@gmail.com>
2023-03-02 17:03:03 -0300

    refactor: add createPost to server exec

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { createPost } from './create-post.js';
 import { createServer } from 'node:http';
 
 const server = createServer(async (req, res) => {
+	await createPost();
+
 	res.end();
 });
 
 server.listen(3000);
-
-setInterval(createPost, 1000 * 60 * 3);
